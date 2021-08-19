409ce4f9ccaa1891a6a7410d745569656b2ab0f0
wenjunnie <1287018840@qq.com>
2021-08-20 01:17:12 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import robots from './mockdata/robots.json'
+import Robot from './components/Robot'
+
+function App() {
+  return (
+    <ul>
+      {robots.map(r => <Robot id={r.id} name={r.name} email={r.email}/>)}
+    </ul>
+  );
+}
+
+export default App;
