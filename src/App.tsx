9f4cb44dd2e5aedc43f50bbaa3d6fb6b0262e299
wenjunnie <1287018840@qq.com>
2021-10-08 01:12:32 +0800

    配置React的CSS模组

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <ul>
-      {robots.map(r => <Robot id={r.id} name={r.name} email={r.email}/>)}
-    </ul>
-  );
+    return (
+        <div className={styles.app}>
+            <div className={styles.robotList}>
+                {robots.map(r => <Robot id={r.id} name={r.name} email={r.email}/>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
